4d49a82a75e3f04a820ded8b48e66933eb7ee1e5
Mingi Kim <migni4575@gmail.com>
2022-05-17 02:17:51 +0900

    #6 Sign in API
    - 커맨드 객체 구현
        - SignInRequest.java
        - SignInResponse.java
    - 컨트롤러 로그인 메서드 구현
    - 서비스 로그인 메서드 구현
    - 리파지터리 이메일 속성으로 사용자 조회 인터페이스 정의
    - 사용자 비밀번호 확인 메서드 구현
    
    Resolves: #6
    See also: None

---
+++ src/main/java/io/wisoft/spring/tutorial/project/account/web/dto/res/SignInResponse.java
@@ -0,0 +1,16 @@
+package io.wisoft.spring.tutorial.project.account.web.dto.res;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SignInResponse {
+
+    private Long accountId;
+
+    public SignInResponse(final Long accountId) {
+        this.accountId = accountId;
+    }
+
+}
