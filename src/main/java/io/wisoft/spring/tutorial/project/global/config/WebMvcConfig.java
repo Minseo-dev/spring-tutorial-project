56af13f73c7d85d5a9bb36016841651d6660b799
Mingi Kim <migni4575@gmail.com>
2022-05-17 22:50:39 +0900

    #14 Allow CORS Policy
    - CORS 정책을 허용하도록 Config 소스 파일에서 메서드를 재정의하였습니다.
    
    Resolves: #14
    See also: None

--- src/main/java/io/wisoft/spring/tutorial/project/global/config/WebMvcConfig.java
+++ src/main/java/io/wisoft/spring/tutorial/project/global/config/WebMvcConfig.java
@@ -9,6 +9,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    // TODO: CORS 허용 설정
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3_600);
+    }
 
 }
